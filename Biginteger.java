da15f60541d9a7feda5791693bf366e96f76d3b2
Tanveer Ahmed Nirjhor <36777480+nirjhor01@users.noreply.github.com>
2018-11-15 12:57:59 +0600

    Create Biginteger.java

---
+++ Biginteger.java
@@ -0,0 +1,26 @@
+
+import java.math.BigInteger;
+import java.util.Scanner;
+ 
+ class Main {
+    public static void main(String[] args) {
+       Scanner in=new Scanner (System.in);
+       while(in.hasNext())
+       {
+           int a=in.nextInt();
+           BigInteger b=BigInteger.ONE;
+            //BigInteger B = BigInteger.valueOf(2);
+
+              // System.out.println(B);
+           for(long i=1;i<=a;i++)
+           {
+            
+              b=b.multiply(BigInteger.valueOf(i));
+            
+           }
+           System.out.println(a+"!");
+           System.out.println(b);
+       }
+    }
+ 
+}
